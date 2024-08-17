b2bbe296bd0f6f87fb7ba5b9f2bf671e41ebc7e7
SALILMEHAN <97797412+SALILMEHAN@users.noreply.github.com>
2024-08-17 23:50:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+const element = document.getElementById("myElement");
+const buttons = document.querySelectorAll(".btn");
+const equal = document.getElementById("equal");
+const delet = document.getElementById("delet");
+const reset = document.getElementById("reset");
+
+for (let i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener("click", function () {
+    element.innerHTML += buttons[i].innerHTML;
+    console.log(buttons[i].innerHTML);
+  });
+}
+
+equal.addEventListener("click", function () {
+  console.log(eval(element.innerHTML));
+  element.innerHTML = eval(element.innerHTML);
+});
+
+delet.addEventListener("click", function () {
+  element.innerHTML = element.innerHTML.slice(0, -1);
+});
+
+reset.addEventListener("click", function () {
+  element.innerHTML = "";
+});
